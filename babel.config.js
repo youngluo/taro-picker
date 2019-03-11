4892f7d0567652820dca9fd7a44556d62c738102
young <youngluo@xwfintech.com>
2019-03-11 10:55:54 +0800

    chore(init): init

---
+++ babel.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  presets: [
+    [
+      '@babel/env',
+      {
+        spec: true,
+        useBuiltIns: false
+      }
+    ]
+  ],
+  plugins: [
+    '@babel/plugin-proposal-class-properties',
+    [
+      '@babel/plugin-transform-react-jsx',
+      {
+        pragma: 'Nerv.createElement'
+      }
+    ],
+    ['@babel/plugin-proposal-object-rest-spread']
+  ]
+}
